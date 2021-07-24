9d74ee6b032f72950c89b3c7fc16028933af9e66
Divya <divyamaurya025@gmail.com>
2021-07-25 01:55:55 +0530

    Front end Template completed

---
+++ src/Routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+
+// const routes = [{
+//     path:'/',
+//     Component: HomePage,
+//     exact: true;
+// }]
+const routes = [];
+
+export const Routes = () => (
+    <Router>
+        <Switch>
+            {   routes.map((route, index) => (
+                <Route
+                    key={index}
+                    path={route.path}
+                    exact={route.exact}
+                    >
+                        <route.Component />
+                    </Route>
+            ))}
+        </Switch>
+    </Router>
+)
